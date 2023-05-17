7d42983e6d38039e13ee81e48b632e7a6a34989f
Chazni Katz <Chazni.k9@gmail.com>
2023-05-18 01:30:57 +0200

    Created basic tcp server

---
+++ server/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+)
+
+//Listen
+//Accept
+//Handle connection -> thread
+
+func main() {
+	l, err := net.Listen("tcp", ":8080")
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer l.Close()
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		go connectionHandler(conn)
+	}
+}
+
+func connectionHandler(conn net.Conn) {
+	for {
+		data, err := bufio.NewReader(conn).ReadString('\n')
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println(data)
+	}
+}
